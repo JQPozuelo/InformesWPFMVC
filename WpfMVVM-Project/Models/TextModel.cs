029fad20d3c40f0f708005ae66c10b5e4cec7226
jorge <quechedo@gmail.com>
2021-11-23 20:56:04 +0100

    Agregar archivos de proyecto.

---
+++ WpfMVVM-Project/Models/TextModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMVVM_Project.Models
+{
+    public class TextModel
+    {
+        public string Referencia { set; get; }
+        public string Marca { set; get; }
+
+        public TextModel()
+        {
+            
+        }
+    }
+}
---
+++ WpfMVVM-Project/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfMVVM_Project.Commands;
+using WpfMVVM_Project.Models;
+
+namespace WpfMVVM_Project.ViewModels
+{
+    class MainViewModel
+    {
+        
+        public TextModel textmodel { set; get; }
+
+        public HomeViewModel SelectedViewModel { set; get; }
+
+        public ICommand UpdateViewCommand { set; get;}
+
+
+        public MainViewModel()
+        {
+            textmodel = new TextModel();
+            textmodel.Marca = "Nike";
+            textmodel.Referencia = "1234";
+
+            UpdateViewCommand = new UpdateViewCommand();
+
+            SelectedViewModel = new HomeViewModel();
+        }
+    }
+}
