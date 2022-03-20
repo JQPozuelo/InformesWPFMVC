fd87bfa7ffd58325b5e5e25a73138902abda96bc
jorge <quechedo@gmail.com>
2022-03-20 17:47:04 +0100

    cambios en el modelo de la factura y arreglo de la validacion de cliente vista consultas. 2 H 20/3/

--- WpfMVVM-Project/Commands/FacturaCom/DniClienteConsultCommand.cs
+++ WpfMVVM-Project/Commands/FacturaCom/DniClienteConsultCommand.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using WpfMVVM_Project.Services.DataSet;
 using WpfMVVM_Project.ViewModels;
+using WpfMVVM_Project.Views;
 
 namespace WpfMVVM_Project.Commands.FacturaCom
 {
@@ -20,31 +22,42 @@ namespace WpfMVVM_Project.Commands.FacturaCom
 
         public void Execute(object parameter)
         {
-            if(consultasViewModel.ClienteM != null)
-            {
-                bool funciona = consultasViewModel.updateViewCommand.reportViewModel.GenerarInformePorCliente(consultasViewModel.ClienteM.DNI);
-                if (funciona)
-                {
-                    consultasViewModel.updateViewCommand.Execute("Reports");
-                }
-                else
-                {
-                    MessageBox.Show("Este cliente no tiene facturas", "Facturas", MessageBoxButton.OK, MessageBoxImage.Warning);
-                }
-            }
-            else
+            /*ConsultasView vista = (ConsultasView)parameter;
+            if (vista.cbCliente.SelectedIndex.Equals(-1))
             {
                 MessageBox.Show("Debes seleccionar un cliente", "Facturas", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
+            else
+            {*/
+                string dni = DataSetHandler.GetDataByDniC(consultasViewModel.ClienteM.DNI);
+                if (dni.Equals(""))
+                {
+                    MessageBox.Show("Debes seleccionar un cliente", "Facturas", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+                else 
+                {
+                    bool funciona = consultasViewModel.updateViewCommand.reportViewModel.GenerarInformePorCliente(consultasViewModel.ClienteM.DNI);
+                    if (funciona)
+                    {
+                        consultasViewModel.updateViewCommand.Execute("Reports");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Este cliente no tiene facturas", "Facturas", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+            //}
            
 
         }
 
         public ConsultasViewModel consultasViewModel { set; get; }
 
+        //public ConsultasView consultasView { set; get; }
         public DniClienteConsultCommand(ConsultasViewModel consultasViewModel)
         {
             this.consultasViewModel = consultasViewModel;
+            //consultasView = new ConsultasView();
         }
     }
 }
